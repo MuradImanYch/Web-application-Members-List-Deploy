22a3606b5b483ed618d76bd8f21c3add7e142b0c
Murad Imanov <mura.imanov@gmail.com>
2023-06-27 09:31:48 +0400

    commit 3

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const db = require('mysql');
 
 const con = db.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: "eu-cdbr-west-03.cleardb.net",
+    user: "bfc35abc76ceef",
+    password: "f3bd1c77",
+    database: "heroku_c4bd331d2ca18ef"
 });
 con.connect(err => {
     if(err) throw err;
